75e079ccccff855f83af3c820205622b69f93f7d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-02 16:57:14 +0000

    Add iterateThroughObject function

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {string[]} reportWithIterator
+ * @returns {string}
+ */
+
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.join(' | ');
+}
