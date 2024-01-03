c23a3b57218893eab8f40d8fcdd10d2c47fef688
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-03 00:54:21 +0000

    Add getFullResponseFromAPI function to 1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,8 @@
+const getFullResponseFromAPI = (success) => new Promise((resolve, reject) => (success
+  ? resolve({
+    status: 200,
+    body: 'Success',
+  })
+  : reject(new Error('The fake API is not working currently'))));
+
+export default getFullResponseFromAPI;
