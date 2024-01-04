bdfc25b788373ae00139ae233f11dcfa9aabd932
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-04 23:34:56 +0000

    Refactor ClassRoom class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,13 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
-  }
-
-  get maxStudentSize() {
-    return this._maxStudentsSize;
-  }
-
-  set maxStudentSize(value) {
-    this._maxStudentsSize = value;
   }
 }
