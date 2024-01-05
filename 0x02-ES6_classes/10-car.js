38eba954903250a34fbc43207ec82c1d2b0116bb
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-05 03:31:29 +0000

    Add Car class with getters, setters, and clone method

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,36 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  cloneCar() {
+    const clonedCar = new this.constructor();
+    return clonedCar;
+  }
+}
