1c4e64ab5887f768797cc666b470c3966a0b8c22
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-09 14:21:39 +0000

    Add Rice to groceries list

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,12 @@
 const groceriesList = () => {
-  return new Map([
+  const map = new Map([
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
+    ['Rice', 1],
     ['Banana', 5],
   ]);
+  return map;
 };
 
 export default groceriesList;
