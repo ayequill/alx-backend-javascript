1899f956aed6b291bd2b071aa201f5d6d4646d1a
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-09 20:29:18 +0000

    Add updateUniqueItems function to manipulate grocery list

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {Map} map
+ */
+
+const updateUniqueItems = (map) => {
+  if (!map instanceof Map) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((val, key) => {
+    if (val === 1) {
+      map.set(key, 100);
+    }
+  });
+};
+
+export default updateUniqueItems;
