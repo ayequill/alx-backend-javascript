c6cf5ad1b7c13665fc22bbe8b2403ba011290d15
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-09 21:45:39 +0000

    Refactor cleanSet function to handle invalid inputs

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,13 +2,15 @@
  *
  * @param {Set} set
  * @param {string} startStr
+ * @returns {string}
  */
 
 export default function cleanSet(set, startStr) {
-  const check = !startStr || !set || !(set instanceof Set);
-  const res = [...set]
+  if (!startStr || !set || !(set instanceof Set)) {
+    return '';
+  }
+  return [...set]
     .filter((val) => val.startsWith(startStr))
     .map((str) => str.slice(startStr.length))
     .join('-');
-  return check ? '' : res;
 }
