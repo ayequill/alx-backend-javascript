e55648eb502f086fe140361c33ae5db802be6227
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-01 00:59:32 +0000

    Add unit tests for calculateNumber function

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('test calculateNumber', () => {
+  it('should return the sum of two numbers', () => {
+    assert.strictEqual(calculateNumber(1.1, 1.1), 2);
+  });
+
+  it('should return a negative sum', () => {
+    assert.strictEqual(calculateNumber(-1.1, -1.1), -2);
+  });
+
+  it('should handle zero', () => {
+    assert.strictEqual(calculateNumber(1.1, 0), 1);
+  });
+});
