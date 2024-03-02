f67e6a99078bb2c0ac0935492a15ce3a4e217245
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-02 00:02:40 +0000

    Refactor readDatabase function in utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,28 +1,27 @@
 const fs = require('fs');
 
-export const readDatabase = async (path) =>
-  new Promise((resolve, reject) => {
-    fs.promises
-      .readFile(path, 'utf8')
-      .then((data) => {
-        const db = data.split('\n');
-        const students = db.slice(1, db.length);
-        const csStudents = students
-          .filter((student) => student.endsWith('CS'))
-          .map((stu) => stu.split(',')[0]);
+const readDatabase = async (path) => new Promise((resolve, reject) => {
+  fs.promises
+    .readFile(path, 'utf8')
+    .then((data) => {
+      const db = data.split('\n');
+      const students = db.slice(1, db.length);
+      const csStudents = students
+        .filter((student) => student.endsWith('CS'))
+        .map((stu) => stu.split(',')[0]);
 
-        const sweStudents = students
-          .filter((student) => student.endsWith('SWE'))
-          .map((stu) => stu.split(',')[0]);
+      const sweStudents = students
+        .filter((student) => student.endsWith('SWE'))
+        .map((stu) => stu.split(',')[0]);
 
-        resolve({
-          CS: csStudents,
-          SWE: sweStudents,
-        });
-      })
-      .catch(() => {
-        reject(new Error('Cannot load the database'));
+      resolve({
+        CS: csStudents,
+        SWE: sweStudents,
       });
-  });
+    })
+    .catch(() => {
+      reject(new Error('Cannot load the database'));
+    });
+});
 
-// module.exports = readDatabase;
+module.exports = readDatabase;
